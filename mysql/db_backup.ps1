bb7423933413869652c9be715be1b8a945dd728c
korosuke613 <korosuke613613@gmail.com>
2019-10-21 16:26:24 +0900

    バックアップ方法を修正

--- mysql/db_backup.ps1
+++ mysql/db_backup.ps1
@@ -1,18 +1,17 @@
 $SNOPY_BACKUP="C:\Users\RPGLocal2018\iCloudDrive\backup\database_snopy"
-$DAYS=5
-
+$DAYS=7
 $file_count = (Get-ChildItem $SNOPY_BACKUP | ? { ! $_.PsIsContainer }).Count
 
-# 古いファイルの削除
-if( $file_count -ge 7 ){
-    Get-ChildItem $SNOPY_BACKUP -Recurse | Where-Object {($_.Mode -eq "-a----") -and ($_.CreationTime -lt (Get-Date).AddDays(-1 * $DAYS))} | Remove-Item -force
+# 古いファイルの削除(10日以上前)
+if( $file_count -ge $DAYS ){
+    Get-ChildItem $SNOPY_BACKUP -Recurse | Where-Object {($_.Mode -eq "-a---l") -and ($_.CreationTime -lt (Get-Date).AddDays(-1 * $DAYS))} | Remove-Item -force
 }
 
 # データベースのバックアップ
 $PORT=3306
 $HOST_NAME="localhost"
 $DAY=Get-Date -UFormat "%Y-%m-%d"
-$FILE_NAME="backup_local" + $DAY + ".dump.gz"
+$FILE_NAME="backup_local" + $DAY + ".dump"
 $FILE_PATH=Join-Path $SNOPY_BACKUP $FILE_NAME
 Write-Output $FILE_PATH
 
@@ -20,8 +19,9 @@ mysqldump `
     -u mysql_user `
     -h $HOST_NAME `
     -P $PORT `
-    --default-character-set=utf8 `
     --single_transaction `
     --max_allowed_packet=200M `
     --column-statistics=0 `
-    --add_drop_table snodb > $FILE_PATH
+    --result-file=$FILE_PATH `
+    --default-character-set=binary `
+    --add_drop_table snodb
