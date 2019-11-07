d45d2f8a2f251968c2ea068eacaf9f9101789b69
korosuke613 <korosuke613613@gmail.com>
2019-11-07 14:20:14 +0900

    習慣バックアップも取るようにした

--- mysql/db_backup.ps1
+++ mysql/db_backup.ps1
@@ -1,10 +1,13 @@
 $SNOPY_BACKUP="C:\Users\RPGLocal2018\iCloudDrive\backup\database_snopy"
-$DAYS=7
-$file_count = (Get-ChildItem $SNOPY_BACKUP | ? { ! $_.PsIsContainer }).Count
 
-# 古いファイルの削除(10日以上前)
+# ここからデイリーバックアップ
+$DAYLY_BACKUP_PATH = Join-Path $SNOPY_BACKUP "daily"
+$DAYS=5
+$file_count = (Get-ChildItem $DAYLY_BACKUP_PATH | ? { ! $_.PsIsContainer }).Count
+
+# 古いファイルの削除($DAYS以上前)
 if( $file_count -ge $DAYS ){
-    Get-ChildItem $SNOPY_BACKUP -Recurse | Where-Object {($_.Mode -eq "-a---l") -and ($_.CreationTime -lt (Get-Date).AddDays(-1 * $DAYS))} | Remove-Item -force
+    Get-ChildItem $DAYLY_BACKUP_PATH -Recurse | Where-Object {($_.Mode -eq "-a---l") -and ($_.CreationTime -lt (Get-Date).AddDays(-1 * $DAYS))} | Remove-Item -force
 }
 
 # データベースのバックアップ
@@ -12,7 +15,7 @@ $PORT=3306
 $HOST_NAME="localhost"
 $DAY=Get-Date -UFormat "%Y-%m-%d"
 $FILE_NAME="backup_local" + $DAY + ".dump"
-$FILE_PATH=Join-Path $SNOPY_BACKUP $FILE_NAME
+$FILE_PATH=Join-Path $DAYLY_BACKUP_PATH $FILE_NAME
 Write-Output $FILE_PATH
 
 mysqldump `
@@ -25,3 +28,19 @@ mysqldump `
     --result-file=$FILE_PATH `
     --default-character-set=binary `
     --add_drop_table snodb
+
+
+# ここからウィークリーバックアップ
+$today_week = (Get-Date).DayOfWeek
+$BACKUP_WEEK = "Thursday"
+$WEEKLY_BACKUP_PATH = Join-Path $SNOPY_BACKUP "weekly"
+
+# 古いファイルの削除($DAYS以上前)
+if( $file_count -ge 30 ){
+    Get-ChildItem $WEEKLY_BACKUP_PATH -Recurse | Where-Object {($_.Mode -eq "-a---l") -and ($_.CreationTime -lt (Get-Date).AddDays(-1 * $DAYS))} | Remove-Item -force
+}
+
+if( $today_week -eq $BACKUP_WEEK){
+    Copy-Item $FILE_PATH $WEEKLY_BACKUP_PATH
+}
+
